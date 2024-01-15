f68918c32b1918dbcc5a8aa81aa9429dca7fc7fb
akzvitor <vitor_eterno@outlook.com>
2024-01-15 13:57:46 -0300

    starting javascript module

---
+++ modulo_js/browser_resources/index.js
@@ -0,0 +1,13 @@
+let name = "Vitor"
+
+console.log(name)
+console.error(name)
+console.warn(name)
+
+/*
+    window.alert(name)
+    window.prompt("Insira seu nome")
+*/
+let confirm = window.confirm("Confirma essa ação?")
+
+console.log(confirm)
