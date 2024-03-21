f973fe2734ee1b115c4402d0e595673140cd3f4e
akzvitor <vitor_eterno@outlook.com>
2024-03-20 21:14:12 -0300

    add comments to document and future review

--- modulo_js/03-DOM/01-events_on_html/index.js
+++ modulo_js/03-DOM/01-events_on_html/index.js
@@ -1,7 +1,15 @@
+/**
+ * events are dynamic way of executing js in a html page
+ * execute js without reload the page
+ * they're activated by specific actions on the page
+ */
+
+// is triggered when the mouse simply passes over the text box
 function onMouseEnter() {
     console.log('mouse enter')
 }
 
+// triggered on click
 function onClick() {
     console.log('clique no elemento')
 }
--- modulo_js/03-DOM/02-obtaining_html_elements/index.js
+++ modulo_js/03-DOM/02-obtaining_html_elements/index.js
@@ -1,19 +1,34 @@
+/**
+ * DOM allows to read specific elements of the html in different ways
+ */
+
 function show() {
+    // get just one element by the specified ID
     const contactList = document.getElementById('contact-list')
     console.log(contactList)
 
+    // get all elements of the specified HTML tag, in this case, 'li' elements
+    // HTMLCollection format
     const listElements = document.getElementsByTagName('li')
     console.log(listElements)
 
+    // get all elements of the specified class
+    // HTMLCollection format
     const contactInputs = document.getElementsByClassName('contact-input')
     console.log(contactInputs)
 
+    // get the selected elements in a NodeList format
+    // this method uses the selector the same way as css
     const contacts = document.querySelectorAll('#contact-list > li > label')
     console.log(contacts)
 
+    // get all elements that have the specified name
+    // NodeList format
     const contact1 = document.getElementsByName('contact1')
     console.log(contact1)
 
+    // get just one selected element
+    // this method also uses the selector the same way as css
     const firstContact = document.querySelector('#contact-list > li > label')
     console.log(firstContact)
 }
